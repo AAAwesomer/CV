6ce475afdfa6fb475382ba90df8656ab19f3cd11
aarni <aarni.alasaarela@gmail.com>
2019-03-14 13:47:43 +0200

    added delay to redirect in order to fade out navicons first

--- src/components/navicons.js
+++ src/components/navicons.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import { SchoolSVG, GearSVG, VideoSVG, AwardSVG } from './svgs.js'
 import Fade from 'react-reveal/Fade';
 
@@ -7,27 +7,49 @@ var createReactClass = require('create-react-class');
 
 var NavIcons = createReactClass({
 
+  getInitialState(){
+    return({redirect: '',
+            show_icons: false})
+  },
+
+  componentDidMount(){
+    this.setState({show_icons: true})
+  },
+
+  go(to){
+    this.setState({show_icons: false})
+    setTimeout(() => {this.props.history.push(to)}, 1000)
+  },
+
   render() {
+
+    /* var redirect;
+    if (this.state.redirect) {
+      redirect = <Redirect to={this.state.redirect} />
+    } */
+
     return (
       <nav>
-        <Fade big top when={this.props.show_icons} duration={1000}>
-          <Link to="/skills"><GearSVG /></Link>
+        <Fade big top when={this.state.show_icons} duration={1000}>
+        <div onClick={this.go.bind(this, '/skills')}><GearSVG /></div>
         </Fade>
 
-        <Fade big left when={this.props.show_icons} duration={1000}> 
-          <Link to="/achievements"><AwardSVG /></Link>
+        <Fade big left when={this.state.show_icons} duration={1000}> 
+        <div onClick={this.go.bind(this, '/achievements')}><AwardSVG /></div>
         </Fade>
 
-        <Fade big right when={this.props.show_icons} duration={1000}>
-        <Link to="/education"><SchoolSVG /></Link>
+        <Fade big right when={this.state.show_icons} duration={1000}>
+          <div onClick={this.go.bind(this, '/education')}><SchoolSVG /></div>
         </Fade>
         
-        <Fade big bottom when={this.props.show_icons} duration={1000}>
-          <Link to="/video"><VideoSVG /></Link>
+        <Fade big bottom when={this.state.show_icons} duration={1000}>
+        <div onClick={this.go.bind(this, '/video')}><VideoSVG /></div>
         </Fade>
+
+        {/* redirect */}
       </nav>
     );
   }
 })
 
-export default NavIcons;
+export default withRouter(NavIcons);
