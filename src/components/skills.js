c1823a6e3b93720eefd22642c45e9667828fa760
aarni <aarni.alasaarela@gmail.com>
2019-03-15 14:48:57 +0200

    added skills section

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import Deck from './cards/cards'
 
-var Skills = () => {
-  return(
-    <div>
-      <h1>Skills</h1>
-    </div>
-  )
-}
+var createReactClass = require('create-react-class');
+
+var Skills = createReactClass({
+  render() {
+    return(
+      <div className='skills'>
+        <Deck />
+      </div>
+    )
+  }
+})
 
 export default Skills
+
