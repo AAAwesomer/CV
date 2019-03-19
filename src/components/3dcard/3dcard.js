a975152ede1869b488de98fc60fc89b035c00152
aarni <aarni.alasaarela@gmail.com>
2019-03-19 13:35:36 +0200

    changed card text class name to threedcard-text in order to differentiate from notecards

--- src/components/3dcard/3dcard.js
+++ src/components/3dcard/3dcard.js
@@ -2,18 +2,22 @@ import React from 'react'
 import { useSpring, animated } from 'react-spring'
 import './3dcard.css'
 
-const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
+const calc = (x, y) => [(y - window.innerHeight / 2) / 80, -(x - window.innerWidth / 2) / 80, 1.05]
 const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
 
-function ThreeDCard() {
-  const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
+const ThreeDCard = props => {
+  const [config, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
   return (
     <animated.div
       className="card"
       onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
       onMouseLeave={() => set({ xys: [0, 0, 1] })}
-      style={{ transform: props.xys.interpolate(trans) }}
-    />
+      style={{ transform: config.xys.interpolate(trans) }}
+    >
+      <div className='threedcard-text'>
+        {props.text}
+      </div>
+    </animated.div>
   )
 }
 
