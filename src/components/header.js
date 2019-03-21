3e420d43d3c9f52b85ebcc54d5fdddbbed08cc18
aarni <aarni.alasaarela@gmail.com>
2019-03-21 14:55:50 +0200

    added name and title to header

--- src/components/header.js
+++ src/components/header.js
@@ -29,18 +29,23 @@ var Header = createReactClass({
     this.setState({ show_icons: false,
                     show_footer: false})
                     
-    setTimeout(() => {this.props.history.push(to)}, 500)
+    setTimeout(() => {this.props.history.push(to)}, 600)
   },
 
   render() {
 
     return (
       <header className="App-header">
-        <Fade top when={this.state.show_icons} duration={1000} distance={'0.2em'}>
+        <Fade top when={this.state.show_icons} duration={600} distance={'0.2em'}>
           <img className='profile' src={profile} alt="Profile" />
         </Fade>
-        <Fade bottom when={this.state.show_icons} duration={1000} distance={'0.2em'}>
+        <Fade bottom when={this.state.show_icons} duration={600} distance={'0.2em'}>
           <NavIcons redirect={this.redirect}/>
+
+          <div className="header-text">
+            <div className="name">Aarni Alasaarela</div>
+            <div className="title">Deep Learning Engineer</div>
+          </div>
         </Fade>
 
         <Footer show_footer={this.state.show_footer}/> 
