690723d673ecef0c0ba5bd272bf7526b5f855f04
aarni <aarni.alasaarela@gmail.com>
2019-03-24 13:39:54 +0200

    credits for new icon

--- src/components/credits.js
+++ src/components/credits.js
@@ -9,7 +9,7 @@ class Credits extends Component {
           <li>Bulb icon made by <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></li>
           <li>Medal icon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></li>
           <li>Graduation hat icon made by <a href="https://www.flaticon.com/authors/zlatko-najdenovski" title="Zlatko Najdenovski">Zlatko Najdenovski</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></li>
-          <li>Play button icon made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></li>
+          <li>Video and mail icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></li>
         </ul>
       </div>
     )
