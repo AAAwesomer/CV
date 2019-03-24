3c372494484192c7f3ab0e916e041f1e2dde12f0
aarni <aarni.alasaarela@gmail.com>
2019-03-24 22:48:24 +0200

    implemented visual part of copy()

--- src/components/header.js
+++ src/components/header.js
@@ -15,7 +15,8 @@ var Header = createReactClass({
 
   getInitialState(){
     return({show_icons: false,
-            show_footer: false})
+            show_footer: false,
+            show_copied: false})
   },
 
   componentDidMount(){
@@ -28,22 +29,34 @@ var Header = createReactClass({
   // hides icons and footer before redirect
   redirect(to){
     this.setState({ show_icons: false,
-                    show_footer: false})
+                    show_footer: false,
+                    show_copied: false})
                     
     setTimeout(() => {this.props.history.push(to)}, 600)
   },
 
   copy(){
-    console.log('hi')
+    clearTimeout(this.timeout)
+    this.setState({show_copied: true})
+    this.timeout = setTimeout(() => {this.setState({show_copied: false})}, 5000)
+  },
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout)
   },
 
   render() {
 
     return (
       <header className="App-header">
+        <Fade when={this.state.show_copied} duration={600}>
+          <div className="copied">Copied to clipboard!</div>
+        </Fade>
+
         <Fade top when={this.state.show_icons} duration={600} distance={'0.2em'}>
           <img className='profile' src={profile} alt="Profile" />
         </Fade>
+
         <Fade bottom when={this.state.show_icons} duration={600} distance={'0.2em'}>
           <NavIcons redirect={this.redirect}/>
 
