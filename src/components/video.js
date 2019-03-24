8a1c38a28bb86c05ca8581adf206d6b0eb8e6ded
aarni <aarni.alasaarela@gmail.com>
2019-03-24 17:12:58 +0200

    implemented and styled initial version of video page

--- src/components/video.js
+++ src/components/video.js
@@ -1,11 +1,42 @@
 import React from 'react'
 
-var Video = () => {
-  return(
-    <div>
-      <h1>Video</h1>
-    </div>
-  )
-}
+var createReactClass = require('create-react-class');
+
+var Video = createReactClass({
+  getInitialState(){
+    return({showNew: true})
+  },
+
+  showNew(){
+    this.setState({showNew: true})
+  },
+
+  showOld(){
+    this.setState({showNew: false})
+  },
+
+  render(){
+    var frame = <iframe title="CWideo2017" width="560" height="315" src="https://www.youtube.com/embed/7a1qOxTC5E8" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    if (this.state.showNew) {
+      frame = <iframe title="CWideo2019" width="560" height="315" src="https://www.youtube.com/embed/OJJVzHpAl08" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    }
+
+    return(
+      <div className='video'>
+        <div className='video-wrapper'>
+          {frame}
+        </div>
+        <div className='video-select'>
+          <div className='video-btn'>
+            <button className='button' onClick={this.showNew}>2019</button>
+          </div>
+          <div className='video-btn'>
+            <button className='button' onClick={this.showOld}>2017</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
 
 export default Video
